fcdce5aac22b640fbd337b207bdd6b60471f3db4
artem <art.holodilov@gmail.com>
2026-07-17 18:57:13 +0300

    I suppose LogHelper is done

---
+++ README.md
@@ -0,0 +1,11 @@
+## SUDOKU Terminal Game. Version 0.1
+
+#### Key features:
+
+1. generate 9x9 sudoku field of several difficulty levels: easy, normal, hard.
+
+2. user can solve sudoku and put some numbers into cells. Errors and score are monitored.
+
+3. autoplay mode, when a bot solves sudoku automatically with several approaches.
+
+4. TUI or GUI are coming soon.
--- src/LogHelper.cpp
+++ src/LogHelper.cpp
@@ -1,26 +1,40 @@
 #include "../include/LogHelper.hpp"
 #include <chrono>
+#include <sstream>
 
 void LogHelper::statusMessage(const std::string& msg) {
-    ofs_ << "STATUS " << msg << "\n";
+    ofs_ << getLocalDateTimeString() << "STATUS: " << msg << "\n";
 }
 
 void LogHelper::warningMessage(const std::string& msg) {
-
+    ofs_ << getLocalDateTimeString() << "WARNING: " << msg << "\n";
 }
 
 void LogHelper::errorMessage(const std::string& msg) {
-
+    ofs_ << getLocalDateTimeString() << "ERROR: " << msg << "\n";
 }
-
 
 std::string LogHelper::getLocalDateTimeString() {
     auto timeNow = std::chrono::system_clock::now();
-    std::chrono::zoned_time localTimeNow("Asia/Irkutsk", timeNow);
-    auto yearMonthDay = std::chrono::year_month_day(floor<days>(localTimeNow.get_local_time()));
-    auto hourMinuteSecond = std::chrono::hh_mm_ss(localTimeNow.get_local_time() - floor<days>(
-    
-     
+    auto localTimeNow = std::chrono::zoned_time("Asia/Irkutsk", timeNow).get_local_time();
+
+    auto yearMonthDay = std::chrono::year_month_day(floor<days>(localTimeNow));
+    auto hourMinuteSecond = std::chrono::hh_mm_ss(localTimeNow - floor<days>(localTimeNow));
+
+    int year = static_cast<int>(yearMonthDay.year());
+    int month = static_cast<int>(yearMonthDay.month());
+    int day = static_cast<int>(yearMonthDay.day());
+    int hour = hourMinuteSecond.hours().count();
+    int minute = hourMinuteSecond.minutes().count();
+    int second = hourMinuteSecond.seconds().count();
+
+    std::ostringstream oss;
+    oss << std::setfill('0');
+    oss << std::setw(4) << year << "-";
+    oss << std::setw(2) << month << "-";
+    oss << std::setw(2) << day << " ";
+    oss << std::setw(2) << hour << ":";
+    oss << std::setw(2) << minute << ":";
+    oss << std::setw(2) << second;
+    return oss.str();
 }
-
-
